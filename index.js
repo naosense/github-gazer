8584bbb9834c177e29f0057b21a55c9535554912
pingao <pingao777@gmail.com>
2018-09-06 11:40:46 +0800

    添加activity和relation关系图

---
+++ index.js
@@ -0,0 +1,39 @@
+var parse_query = function (query_string) {
+
+    if (is_empty(query_string)) {
+        return {};
+    }
+    var query = {};
+    query_string = query_string.charAt(0) === '?' ? query_string.substring(1) : query_string;
+    console.log(query_string);
+    var query_array = query_string.split('&');
+    for (var i in query_array) {
+        var kv = query_array[i].split('=');
+        console.log(kv);
+        query[kv[0]] = kv[1];
+    }
+    return query;
+
+};
+
+var is_empty = function (s) {
+    return s === null || s === undefined || s === '';
+};
+
+var query_github_api = function (url) {
+    var result = {};
+    $.ajax({
+        headers: {
+            Accept: 'application/vnd.github.v3.star+json; charset=utf-8'
+        },
+        url: url,
+        dataType: 'json',
+        type: 'get',
+        async: false,
+        success: function (data) {
+            result = data;
+        }
+    });
+
+    return result;
+};
